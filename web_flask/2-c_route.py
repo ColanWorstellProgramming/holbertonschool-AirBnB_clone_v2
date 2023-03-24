5a6b45fa8c098829edde79dd01854cf33fc3fe9c
Colan Worstell <Cworstellprogramming@gmail.com>
2023-03-24 16:32:20 -0700

    ?

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -20,7 +20,7 @@ def hnbn():
 
 @app.route('/c/<text>', strict_slashes=False)
 def C_OP(inp):
-    return "C " + inp.replace("_", " ")
+    return "name", "C " + inp.replace("_", " ")
 
 
 if __name__ == '__main__':
