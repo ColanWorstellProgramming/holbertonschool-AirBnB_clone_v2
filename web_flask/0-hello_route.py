fe6210c3d0d966589575387bdfb9d111ab6038f7
Colan Worstell <Cworstellprogramming@gmail.com>
2023-03-24 15:39:19 -0700

    fix

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
 '''Flask With Python'''
-
 from flask import Flask, request
 
+
 app = Flask(__name__)
+
 
 @app.route('/', strict_slashes=False)
 def startup():
     name = request.args.get("name", "Hello HBNB!")
     return name
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
