0b70919c098ed1c5c862a6efacc8dbc93554bc9c
Colan Worstell <Cworstellprogramming@gmail.com>
2023-03-24 15:28:44 -0700

    0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-"""placeholder"""
-from flask import Flask
+'''Flask With Python'''
+
+from flask import Flask, request
 
 app = Flask(__name__)
 
-
 @app.route('/', methods=['GET'], strict_slashes=False)
-def hello_hbnb():
-    return "Hello HBNB!"
-
+def startup():
+    name = request.args.get("name", "Hello World")
+    return name
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
