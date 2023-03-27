bb0f452b7d388711916a5a961322709df0f9b8ec
Colan Worstell <Cworstellprogramming@gmail.com>
2023-03-26 17:34:20 -0700

    6

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -1,45 +1,43 @@
 #!/usr/bin/python3
-"""placeholder"""
-from flask import render_template, Flask
+'''Flask With Python'''
+from flask import Flask, request, render_template
+
 
 app = Flask(__name__)
 
 
-@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
-def num_mod_two(n):
-    return render_template('6-number_odd_or_even.html', n=n)
+@app.route('/', strict_slashes=False)
+def hellohnbn():
+    name = request.args.get("name", "Hello HBNB!")
+    return name
 
 
-@app.route('/number_template/<int:n>', strict_slashes=False)
-def num_temp(n):
-    return render_template('5-number.html', n=n)
-
-
-@app.route('/number/<int:n>', methods=['GET'], strict_slashes=False)
-def num_int(n):
-    return "{} is a number".format(n)
-
-
-@app.route('/', methods=['GET'], strict_slashes=False)
-def holla_hbnb():
-    return "Hello HBNB!"
-
-
-@app.route('/hbnb', methods=['GET'], strict_slashes=False)
-def hbnbbb():
-    return "HBNB"
-
-
-@app.route('/c/<text>', methods=['GET'], strict_slashes=False)
-def c_is_meh(text):
-    return "C " + text.replace("_", " ")
+@app.route('/hbnb', strict_slashes=False)
+def hnbn():
+    name = request.args.get("name", "HBNB")
+    return name
 
 
 @app.route('/python/', defaults={'text': 'is cool'},
            methods=['GET'], strict_slashes=False)
 @app.route('/python/<text>', methods=['GET'], strict_slashes=False)
-def python_awesome(text):
+def C_OP(text):
     return "Python " + text.replace("_", " ")
+
+
+@app.route('/number/<int:n>', methods=['GET'], strict_slashes=False)
+def ints(n):
+    return "{} is a number".format(n)
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def ints_n_temps(n):
+    return render_template('5-number.html', n=n)
+
+
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+def even_or_oddities(n):
+    return render_template('6-number_odd_or_even.html', n=n)
 
 
 if __name__ == '__main__':
